64666416e0ad6f18b5792ae5bc84d756d1b8aaaf
Mehdi Raza <mehdi.reza@gmail.com>
2019-08-14 19:05:37 +0500

    doWithEntityManager with Function<EntityManager, R>

--- src/main/java/org/palestyn/transaction/TransactionManager.java
+++ src/main/java/org/palestyn/transaction/TransactionManager.java
@@ -1,10 +1,12 @@
 package org.palestyn.transaction;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 public interface TransactionManager {
 	public void doWithEntityManager(Transactional.TxType scope, Consumer<EntityManager> callback);
+	public <R> R doWithEntityManager(Transactional.TxType scope, Function<EntityManager, R> callback);
 }
